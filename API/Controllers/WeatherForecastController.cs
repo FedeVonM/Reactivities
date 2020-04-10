a9f806b0561741f4f620f00ef5ed45f63fc22ad6
fedevonm <fedevm@mbp-de-federico.lan>
2020-04-10 17:10:21 -0300

    Initial Commit

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly DataContext _contex;
+
+        public WeatherForecastController(DataContext contex)
+        {
+            this._contex = contex;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _contex.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int id)
+        {
+            var values = await _contex.Values.FindAsync(id);
+            return Ok(values);
+        }
+    }
+}
+ 
---
+++ Domain/Auto.cs
@@ -0,0 +1,8 @@
+namespace Domain
+{
+    public class Auto
+    {
+        public int Id{ get; set; }
+        public string Modelo { get; set; }
+    }
+}
