6cef0315a4224b32174a9b7135acdfadc56f93f0
fedevonm <fedevm@mbp-de-federico.lan>
2020-04-10 23:40:02 -0300

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,9 +28,18 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(opt =>
-           {
-               opt.UseMySql(Configuration.GetConnectionString("ReactivitiesDBConnection"));
-           });
+            {
+                opt.UseMySql(Configuration.GetConnectionString("ReactivitiesDBConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+
+
+                });
+            });
             services.AddControllers();
         }
 
@@ -47,6 +56,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
